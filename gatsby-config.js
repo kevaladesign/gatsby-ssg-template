1a83fd9572e725592f602de04c69f878b8f14ff9
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-09-21 22:46:56 -0700

    build: add support for environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
+
+console.log(`Environment: ${activeEnv}`);
+require('dotenv').config({ path: `.env.${activeEnv}` });
+
 module.exports = {
   siteMetadata: {
     title: `[WEBSITE TITLE]`,

010f94aae994bb724568db258aa40528299b315d
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-09-21 21:47:48 -0700

    style: apply prettier formatting to initial files

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement } from "react"
-import { PageProps } from "gatsby"
+import React, { ReactElement } from 'react';
+import { PageProps } from 'gatsby';
 
 const NotFoundPage = (_props: PageProps): ReactElement => (
   <>
     <h1>Not Found Page</h1>
   </>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement } from "react"
-import { PageProps } from "gatsby"
+import React, { ReactElement } from 'react';
+import { PageProps } from 'gatsby';
 
 const IndexPage = (_props: PageProps): ReactElement => (
   <>
     <h1>Index Page</h1>
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;

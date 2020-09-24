0456e01384f9213cdb5cff1ae1128c6989b09d70
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-09-23 18:23:11 -0700

    refactor: remove preflight styles in preparation for twin installation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,3 @@ import { enableAllPlugins } from 'immer';
 // @note Enable Immer
 
 enableAllPlugins();
-
-// @note Import Tailwind CSS Preflight Styles
-
-import './src/shared/styles/preflight.css';
